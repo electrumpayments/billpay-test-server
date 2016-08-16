65862a689cd8785a7f59f4b1db13d3f11e1ccee4
Mordechai Serraf <mserraf34@gmail.com>
2016-08-16 12:04:36 +0200

    local testing on instal
    - Builds a docker and runs the server and runs newman tests against it
    - cleans up docker image and container
    - to skip use -Dmaven.test.skip=true

---
+++ test/postman/run_tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+basedir=$1
+testdir=$2
+echo "Starting docker container"
+docker build -t="billpay-test-server" ${basedir}/target
+docker run -d -p 8080:8080 --name billpay-test-server_container billpay-test-server
+/git/circlecitools/bin/waitForServer.sh localhost:8080 1000
+${testdir}/run_newman.sh ${testdir}
+rc=$?
+echo "Cleaning up Docker"
+docker stop billpay-test-server_container
+docker rm billpay-test-server_container
+docker rmi billpay-test-server
+exit $rc
