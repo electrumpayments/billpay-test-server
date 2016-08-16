793cb6a329c1d8cd591cc14f4a8bcdd1c05fabc5
Mordechai Serraf <mserraf34@gmail.com>
2016-08-16 15:20:17 +0200

    set to turn on tests

--- test/postman/run_tests.sh
+++ test/postman/run_tests.sh
@@ -4,7 +4,7 @@ testdir=$2
 echo "Starting docker container"
 docker build -t="billpay-test-server" ${basedir}/target
 docker run -d -p 8080:8080 --name billpay-test-server_container billpay-test-server
-/git/circlecitools/bin/waitForServer.sh localhost:8080 1000
+/git/circlecitools/bin/waitForServer.sh localhost:8080 5000
 ${testdir}/run_newman.sh ${testdir}
 rc=$?
 echo "Cleaning up Docker"
